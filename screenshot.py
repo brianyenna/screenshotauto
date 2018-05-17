c9b9783d6f8068248741db1ed5120864b273b0f0
Brian <brian@Brians-MacBook-Pro.local>
2018-05-17 17:07:33 +0800

    Got rid of the KeyPressEvent problem

--- screenshot.py
+++ screenshot.py
@@ -1,11 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget
 from PyQt5.QtGui import QPainter, QColor, QPen, QIcon
 from PyQt5.QtCore import Qt, QPoint
 
-class ScreenShot(QMainWindow):
+class ScreenShot(QWidget):
     def __init__(self):
-        QMainWindow.__init__(self)
+        super().__init__()
         #Window settings
         self.left = 50
         self.top = 100
@@ -18,12 +18,13 @@ class ScreenShot(QMainWindow):
     def initUI(self):
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setWindowOpacity(self.opacity)
-        self.setWindowFlags(Qt.FramelessWindowHint | Qt.SplashScreen | Qt.WindowStaysOnTopHint)
+        # self.setWindowFlags(Qt.FramelessWindowHint | Qt.SplashScreen | Qt.WindowStaysOnTopHint)
         self.setAutoFillBackground(True)
         p = self.palette()
         p.setColor(self.backgroundRole(), self.color)
         self.setPalette(p)
         self.setFixedSize(self.width, self.height)
+        self.show()
 
     def keyPressEvent(self, event):
         key = event.key()
@@ -34,7 +35,6 @@ class ScreenShot(QMainWindow):
         if key == Qt.Key_Right:
             print('Right')
             self.move_window()
-
         event.accept()
 
     def move_window(self):
@@ -59,6 +59,5 @@ class ScreenShot(QMainWindow):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     MainWindow = ScreenShot()
-    MainWindow.show()
     MainWindow.terminal_ask()
     sys.exit(app.exec_())
