bfbb4a8960c6d497065c723c12729847c310bd87
Jia Xiaodong <xdjiaxd@gmail.com>
2018-05-17 16:42:45 +0800

    fixed keypress

--- screenshot.py
+++ screenshot.py
@@ -27,51 +27,22 @@ class ScreenShot(QMainWindow):
         self.setFixedSize(self.width, self.height)
 
     def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Left:
+        key = event.key()
+        if key == Qt.Key_Left:
             print("Left")
             self.move_window()
-        if event.key() == Qt.Key_R:
-            print('R')
-        # elif event.key() == Qt.Key_Right:
-        #     self.proceed_right()
+        if key == Qt.Key_Right:
+            print('Right')
+            self.move_window()
         event.accept()
 
-    # def keyPressEvent(self, event):
-    #     key = event.key()
-    #     key_map = {Qt.Key_Left: self.move_window(movement = -1, h = True),
-    #                Qt.Key_Right: self.move_window(movement = 1, h = True),
-    #                Qt.Key_Down: self.move_window(movement = 1, h = False),
-    #                Qt.Key_Up: self.move_window(movement = -1, h = False)}
-    #     if key in key_map:
-    #         key_map[key]
-    #     print('Key detected')
-    #     event.accept()
-
-    def move_window():
-        # if h: #Horizontal movement
-        #     self.width += movement
-        #     self.initUI()
-        # else:
-        #     self.height += movement
-        #     self.initUI()
+    def move_window(self):
         self.width += 1
         self.initUI()
         print('Key Press Event detected')
-
-    def terminal_ask(self):
-        while True:
-            print("If you want to close this window, type stop: ")
-            get_input = input()
-            if get_input.lstrip().rstrip().lower() == "stop":
-                self.close_window()
-            else:
-                print("Invalid Syntax, Try it again.")
-            QApplication.processEvents()
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     MainWindow = ScreenShot()
     MainWindow.show()
-    MainWindow.terminal_ask()
     sys.exit(app.exec_())
