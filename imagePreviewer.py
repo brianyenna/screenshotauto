470da7e4b1d8640f676bae02bd74c3e6dfd144d8
Jia Xiaodong <xdjiaxd@gmail.com>
2018-05-18 14:04:32 +0800

    Added scaling to the QLabel

--- imagePreviewer.py
+++ imagePreviewer.py
@@ -1,11 +1,11 @@
 from PyQt5.QtWidgets import QLabel
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 
 class ImagePreviewer(QLabel):
     def __init__(self, mainWindow):
         super().__init__()
         self.mainWindow = mainWindow
-
     def keyPressEvent(self, event):
         key = event.key()
 
@@ -15,7 +15,12 @@ class ImagePreviewer(QLabel):
             self.mainWindow.clear_screenshot()
         if (key == Qt.Key_Q):
             self.close()
+    
+    def setPixmap(self, pixmap):
+        super().setPixmap(pixmap)
+        self.aspectRatio = pixmap.width() / pixmap.height()
+        self.originalPm = pixmap
 
-
-
+    def resizeEvent(self, event):
+        super().setPixmap(self.originalPm.scaled(event.size().width(), event.size().height(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation))
 
