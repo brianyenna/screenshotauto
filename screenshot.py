0b523f64c492f1961a3160e8d56b4bc76bdd9eb2
Jia Xiaodong <xdjiaxd@gmail.com>
2018-05-18 10:07:10 +0800

    Added more keys and status bar
    Status bar now displays info about the window. Ctrl and Shift keys now increase the power of wasd keys. Try pressing both down.

--- screenshot.py
+++ screenshot.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QLCDNumber, QSlider, QGridLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLCDNumber, QSlider, QGridLayout
 from PyQt5.QtGui import QPainter, QColor, QPen, QIcon, QScreen
 from PyQt5.QtCore import Qt, QPoint
 
@@ -8,6 +8,7 @@ class ScreenShot(QMainWindow):
         super().__init__()
         #Window settings
         self.stepMultiplier = 10
+        self.stepBigMultipler = 10
         self.stepSize = 1 #number of pixels to move the window by
         geometry = app.desktop().availableGeometry() ##
         self.left = 50
@@ -16,36 +17,50 @@ class ScreenShot(QMainWindow):
         self.minHeight = 10
         self.width = 300
         self.height = 300
-
-        # self.height = 200
         self.opacity = 0.5
         self.color = QColor(255,3,3)  #Set RGB colours later
+        self.statusText = "X: {} Y: {} | Width: {} Height: {}"
         self.initUI()
 
     def initUI(self):
         self.refreshWindowGeometry()
         self.setWindowOpacity(self.opacity)
         self.setWindowFlags(Qt.CustomizeWindowHint)
-        self.setAutoFillBackground(True)
+        
         p = self.palette()
         p.setColor(self.backgroundRole(), self.color)
+        self.setAutoFillBackground(True)
         self.setPalette(p)
+        
+        p = self.statusBar().palette()
+        p.setColor(self.backgroundRole(), Qt.white)
+        self.statusBar().setAutoFillBackground(True)
+        self.statusBar().setPalette(p)
+
         self.show()
+
+    def paintEvent(self, event):
+        self.statusBar().clearMessage()
+        self.statusBar().showMessage(self.statusText.format(self.left,self.top,self.width,self.height))
+        self.statusBar().update()
 
     def refreshWindowGeometry(self):
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.show()
+        self.update()
 
     def resizeEvent(self, event):
         self.width = event.size().width()
         self.height = event.size().height()
         QMainWindow.resizeEvent(self, event)
+        self.update()
 
     def keyPressEvent(self, event):
         modifier = QApplication.keyboardModifiers()
         key = event.key()
-        stepSize = (self.stepSize * self.stepMultiplier) if (modifier == Qt.ShiftModifier) else (self.stepSize)
-
+        stepSize = self.stepSize
+        stepSize *= self.stepBigMultipler if (modifier & Qt.ShiftModifier) else 1
+        stepSize *= self.stepMultiplier if (modifier & Qt.ControlModifier) else 1
+        
         #Resize window height and width
         if key == Qt.Key_Left:
             self.resize_window(-stepSize, 0)
@@ -56,15 +71,15 @@ class ScreenShot(QMainWindow):
         if key == Qt.Key_Down:
             self.resize_window(0, stepSize)
 
-        #Resize window reference point (self.top and self.left)
+        #Move window
         if key == Qt.Key_A:
-            self.change_window_reference(-stepSize, 0)
+            self.move_window(-stepSize, 0)
         if key == Qt.Key_D:
-            self.change_window_reference(stepSize, 0)
+            self.move_window(stepSize, 0)
         if key == Qt.Key_W:
-            self.change_window_reference(0, -stepSize)
+            self.move_window(0, -stepSize)
         if key == Qt.Key_S:
-            self.change_window_reference(0, stepSize)
+            self.move_window(0, stepSize)
 
         if key == Qt.Key_C:
             self.preview_screen = QApplication.primaryScreen().grabWindow(0,0,300,300)
@@ -80,7 +95,7 @@ class ScreenShot(QMainWindow):
             self.height = 10
         self.refreshWindowGeometry()
 
-    def change_window_reference(self, inc_x, inc_y):
+    def move_window(self, inc_x, inc_y):
         self.left += inc_x
         self.top += inc_y
         if self.left <= 0:
