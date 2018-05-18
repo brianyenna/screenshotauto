fb21e332e7a850051a5c64866eea40f36fd22b1d
Jia Xiaodong <xdjiaxd@gmail.com>
2018-05-18 09:38:49 +0800

    Changed b key to use modifier
    Now shift+wasd or arrows will increase the move rate instead of using a toggle button

--- screenshot.py
+++ screenshot.py
@@ -3,18 +3,19 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QLCDNumb
 from PyQt5.QtGui import QPainter, QColor, QPen, QIcon, QScreen
 from PyQt5.QtCore import Qt, QPoint
 
-class ScreenShot(QWidget):
+class ScreenShot(QMainWindow):
     def __init__(self):
         super().__init__()
         #Window settings
-        self.big_steps = False #If True, the step_size will be larger (i.e 10)
-        self.step_size = 1 #number of pixels to move the window by
+        self.stepMultiplier = 10
+        self.stepSize = 1 #number of pixels to move the window by
         geometry = app.desktop().availableGeometry() ##
         self.left = 50
         self.top = 100
-        # self.width = 600
-        self.width = geometry.width()
-        self.height = geometry.height()
+        self.minWidth = 10
+        self.minHeight = 10
+        self.width = 300
+        self.height = 300
 
         # self.height = 200
         self.opacity = 0.5
@@ -24,6 +25,7 @@ class ScreenShot(QWidget):
     def initUI(self):
         self.refreshWindowGeometry()
         self.setWindowOpacity(self.opacity)
+        self.setWindowFlags(Qt.CustomizeWindowHint)
         self.setAutoFillBackground(True)
         p = self.palette()
         p.setColor(self.backgroundRole(), self.color)
@@ -40,52 +42,41 @@ class ScreenShot(QWidget):
         QMainWindow.resizeEvent(self, event)
 
     def keyPressEvent(self, event):
+        modifier = QApplication.keyboardModifiers()
         key = event.key()
-        if key == Qt.Key_B:
-            self.big_steps = not self.big_steps #inverts the Boolean value
-
-        if self.big_steps:
-            self.step_size = 15
-        else:
-            self.step_size = 1
+        stepSize = (self.stepSize * self.stepMultiplier) if (modifier == Qt.ShiftModifier) else (self.stepSize)
 
         #Resize window height and width
         if key == Qt.Key_Left:
-            self.resize_window(-self.step_size, 0)
+            self.resize_window(-stepSize, 0)
         if key == Qt.Key_Right:
-            self.resize_window(self.step_size, 0)
+            self.resize_window(stepSize, 0)
         if key == Qt.Key_Up:
-            self.resize_window(0, -self.step_size)
+            self.resize_window(0, -stepSize)
         if key == Qt.Key_Down:
-            self.resize_window(0, self.step_size)
+            self.resize_window(0, stepSize)
 
         #Resize window reference point (self.top and self.left)
         if key == Qt.Key_A:
-            self.change_window_reference(-self.step_size, 0)
+            self.change_window_reference(-stepSize, 0)
         if key == Qt.Key_D:
-            self.change_window_reference(self.step_size, 0)
+            self.change_window_reference(stepSize, 0)
         if key == Qt.Key_W:
-            self.change_window_reference(0, -self.step_size)
+            self.change_window_reference(0, -stepSize)
         if key == Qt.Key_S:
-            self.change_window_reference(0, self.step_size)
+            self.change_window_reference(0, stepSize)
 
         if key == Qt.Key_C:
-            # pixmap = QScreen.grabWindow(self.winId())
-            # p = self.grab()
-            # p.save('test','jpg')
             self.preview_screen = QApplication.primaryScreen().grabWindow(0,0,300,300)
-            # # self.img_preview.setPixmap(self.preview_screen.scaled(350,350, Qt.KeepAspectRatio, Qt.SmoothTransformation))
             self.preview_screen.save('test','jpg')
-            # pixmap = QScreen.grabWindow(QApplication.?
-            # pixmap.save('test','jpg')
 
 
     def resize_window(self, width, height):
         self.width += width
         self.height += height
-        if self.width <= 0:
+        if self.width <= self.minWidth:
             self.width = 10
-        if self.height <= 0:
+        if self.height <= self.minHeight:
             self.height = 10
         self.refreshWindowGeometry()
 
